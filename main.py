60c7f3ec2c44f23c2dba6fe9e6cede87edda5678
Bryan05U <bryanalexis.carcamo@alumnos.ulagos.cl>
2025-11-18 21:06:29 -0300

    añadiendo cosas a realizar

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame as pg
+import sys
+
+# Inicialización de Pygame
+pg.init()
+
+# Configuración de la ventana (800x600)
+PANTALLA = pg.display.set_mode((800,600))
+pg.display.set_caption("Kangaroo Escape") # Título de la ventana
